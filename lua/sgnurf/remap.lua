c0f99d22a252614264cd74574979e5e6bd3b5a57
Xavier Gérin <xavier.gerin@gmail.com>
2024-02-21 21:40:46 +0100

    Initial config

---
+++ lua/sgnurf/remap.lua
@@ -0,0 +1,10 @@
+vim.g.mapleader = " "
+vim.keymap.set("n","<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n","<C-d>", "<C-d>zz")
+vim.keymap.set("n","<C-u>", "<C-u>zz")
+vim.keymap.set("n","n", "nzz")
+vim.keymap.set("n","N", "Nzz")
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set("n","<leader>o", "o<ESC>")
+vim.keymap.set("n","<leader>O", "O<ESC>")
+
